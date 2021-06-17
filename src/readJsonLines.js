76d1027bbd95ba17b052ae515d5c53fa61ac4524
gangeshwari-rajavelu <gangeshwari.rajavelu@freshworks.com>
2021-06-18 00:12:31 +0530

    Adding checks to avoid 'Uncaught SyntaxError: Unexpected end of input'

--- src/readJsonLines.js
+++ src/readJsonLines.js
@@ -1,6 +1,8 @@
 function readJsonLines(string) {
   return string.split('\n')
     .filter(line => line)
+    .filter(line => line.startsWith('{')
+    .filter(line => line.endsWith('}')
     .map(line => line.trim())
     .map(line => JSON.parse(line));
 }
